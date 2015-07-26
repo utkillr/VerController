b7b85b58c405dc2ec0537c3514afadddec4461ed
utkillr <nosikov-k@mail.ru>
2015-07-26 16:46:50 +0300

    Update Modernizing.h

--- Modernizing.h
+++ Modernizing.h
@@ -1,1 +1,23 @@
+#ifndef MODERNIZING
+#define MODERNIZING
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "structures.h"
+#include "Deleting.h"
+#include "outputs.h"
+#include "FileChecking.h"
+#include "PermChecking.h"
+
+using namespace std;
+
+//Modernize file to needed version by path
+void modernizeFile(string fileName, vector<diffNode*> path);
+//Modernize vector of diffLines using diffNode difference
+vector<diffString> getDiffChanges(vector<diffString> fileStrings, diffNode* difference);
+//Change function for main()
+bool changeFileVersion(string fileName, string nickname);
+
+#endif
