a39d50949f56055532da14f69e635b88f829a0a4
utkillr <nosikov-k@mail.ru>
2015-07-26 16:39:15 +0300

    AddingPermissions.h
    Functions that allow to add new permissions (actually, add new users for file)

---
+++ AddingPermissions.h
@@ -0,0 +1,15 @@
+#ifndef ADDINGPERMISSIONS
+#define ADDINGPERMISSIONS
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Adding.h"
+#include "PermChecking.h"
+
+//Adding new user
+bool addPermission(string fileName, string owner, string op);
+
+#endif
