9c9e5be0ca6d17d879ca9473609de48ef1ab552f
utkillr <nosikov-k@mail.ru>
2015-07-26 16:44:04 +0300

    Help.h
    Functions for Help option (for those who don't understand how to work with this program)

---
+++ Help.h
@@ -0,0 +1,24 @@
+#ifndef HELPFILE
+#define HELPFILE
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+//Common function for help
+void printHelp();
+//Help for adding option
+void showAddingHelp();
+//Help for adding existing file option
+void showAddingExistingHelp();
+//Help for adding permissions option
+void showAddingPermissionsHelp();
+//Help for deleting option
+void showDeletingHelp();
+//Help for checking files option
+void showCheckingHelp();
+//Help for go to version option
+void showGoToVersionHelp();
+
+#endif
